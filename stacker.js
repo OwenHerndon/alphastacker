71ebd0da8243ccafef63e1f4a12d247b3ce0336d
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-08-01 18:03:59 -0500

    added function

---
+++ stacker.js
@@ -0,0 +1,67 @@
+
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+//var letters = {
+//	letterArray : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//}
+//console.log("the alphabet: ", letters);
+
+//for (var i = 0; i < letters.length; i++) {
+//	console.log(letters[i]);
+//}
+
+function stackLetters(){
+	return letters.map(function(1,1){
+		return letters;
+	}
+}
+
+console.log(stackLetters());
+
+var currentEle = letters.slice(0,1);
+console.log("the current element : ", currentEle);
+
+//var display = new currentEle();
+//console.log("the new array ", display);
+//splice first element from letter array
+//splice that element into new array
+//display new array to console
+
+//function stackLetters(getLetters){
+//	for (var i = 0; i < letters.length; i++) {
+//		console.log(letters.slice(0,1));
+		//return(letters.slice(i,1);
+		//letters.shift();
+		//letters++;
+		//return(letters.[i] + letters[1] + letters[2]);
+//	}
+//}
+
+//function stackLetters(){
+//	console.log(letters.shift);
+//	letters.push;
+//}
+
+//console.log(stackLetters(letters));
+
+
+//var currentEle = stackLetters(letters); 
+//console.log(addEle(currentEle));
+
+//var addLet = function(letters){
+//for (var i = 0; i < letters.length; i++) {
+		//console.log(letters[i]);
+//		return(letters[i]);
+//}
+
+
+
+//var addMoreLetters = 
+
+//var for competed display element
+//function add new element to displayed elements
+
+//for (var i = 0; i < letters.length; i++) {
+	//console.log(alphaStacker(letters[i]));
+	//letters++;
+//}
